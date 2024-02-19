475349cae84f9fc20d0d8c040f3f7fbf576e3ff7
gjing <georgejing93@gmail.com>
2024-02-18 22:34:10 -0800

    basic api call

--- demo-app/src/app/app.component.ts
+++ demo-app/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { BarComponent } from './bar/bar.component';
+import { CryptochartComponent } from './cryptochart/cryptochart.component';
+import { CryptoApiService } from './crypto-api.service';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, BarComponent, CryptochartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'demo-app';
+  constructor(private cryptoApiService: CryptoApiService) {}
+
+  ngOnInit() {
+    this.cryptoApiService.get_test().subscribe(
+      (response) => { console.log(response); },
+      (error)=> { console.log(error); }
+    )
+  }
 }
