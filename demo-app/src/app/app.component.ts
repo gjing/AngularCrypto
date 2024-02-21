e22c58fc778e2a75981259abd0c9813966d42f07
gjing <georgejing93@gmail.com>
2024-02-20 19:42:55 -0800

    d3js include

--- demo-app/src/app/app.component.ts
+++ demo-app/src/app/app.component.ts
@@ -1,24 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BarComponent } from './bar/bar.component';
 import { CryptochartComponent } from './cryptochart/cryptochart.component';
-import { CryptoApiService } from './crypto-api.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BarComponent, CryptochartComponent],
+  imports: [RouterOutlet, CryptochartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'demo-app';
-  constructor(private cryptoApiService: CryptoApiService) {}
-
-  ngOnInit() {
-    this.cryptoApiService.get_test().subscribe(
-      (response) => { console.log(response); },
-      (error)=> { console.log(error); }
-    )
-  }
 }
